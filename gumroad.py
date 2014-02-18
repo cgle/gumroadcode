ce0d90070ab4b51cc11e27d72799b00b10de4341
cgle <cle@macalester.edu>
2014-02-18 09:20:01 -0600

    2nd commit
    fix edges

--- gumroad.py
+++ gumroad.py
@@ -1,5 +1,5 @@
 from collections import defaultdict
-clist =  [8,1,3,9]
+clist =  [0,0]
 
 def pivot(clist):
     if len(clist)==1:
@@ -9,16 +9,26 @@ def pivot(clist):
     else:
         visited=defaultdict(int)
         p=len(clist)/2
-        visited[p]+=1
         front=clist[:p]
         end=clist[p+1:]
-        while len(front)>=1 and len(end)>=1 and visited[p]<=1:
-            if sum(front)<sum(end):
-                p+=len(end)/2
-            elif sum(front)>sum(end):
-                p-=len(front)/2
+        while visited[p]<=1:
+            add_var=0
+            if len(front)==1 or len(end)==1:
+                add_var=1
+            if clist[p]>=0:
+                if sum(front)<sum(end):
+                    p+=len(end)/2+add_var
+                elif sum(front)>sum(end):
+                    p-=len(front)/2-add_var
+                else:
+                    return p
             else:
-                return p
+                if sum(front)<sum(end):
+                    p-=len(front)/2-add_var
+                elif sum(front)>sum(end):
+                    p+=len(end)/2+add_var
+                else:
+                    return p
             visited[p]+=1
             front=clist[:p]
             end=clist[p+1:]
